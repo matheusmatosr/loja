d1018cd17fc36ecd9b2f7a69ee2310fad2ed5dc7
Matheus Matos <100291312+matheusmatosr@users.noreply.github.com>
2023-01-27 08:51:53 -0300

    commit

---
+++ src/data.js
@@ -0,0 +1,31 @@
+import img1 from './img/img1.jpeg';
+import img2 from './img/img2.jpeg';
+import img3 from './img/img3.jpeg';
+
+const data = {
+    productData: [
+        {
+            id: 1,
+            img: img1,
+            title: 'Uva',
+            desc: '',
+            price: 5
+        },
+        {
+            id: 2,
+            img: img2,
+            title: 'Morango',
+            desc: '',
+            price: 7
+        },
+        {
+            id: 3,
+            img: img3,
+            title: 'Laranja',
+            desc: '',
+            price: 13
+        }
+    ],
+};
+
+export default data;
